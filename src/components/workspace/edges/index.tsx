f46f21cbfb26c25a7475cf7f57d8913751f99d33
beejsbj <burooj.bj@gmail.com>
2025-01-20 21:22:20 -0500

    Edges fix

--- src/components/workspace/edges/index.tsx
+++ src/components/workspace/edges/index.tsx
@@ -3,9 +3,9 @@ import type { Edge, EdgeTypes } from "@xyflow/react";
 const defaultEdgeOptions = {};
 
 export const initialEdges: Edge[] = [
-  { id: "a->c", source: "a", target: "c", animated: true },
-  { id: "b->d", source: "b", target: "d" },
-  { id: "c->d", source: "c", target: "d" },
+  //   { id: "a->c", source: "a", target: "c", animated: true },
+  //   { id: "b->d", source: "b", target: "d" },
+  //   { id: "c->d", source: "c", target: "d" },
 ];
 
 export const edgeTypes = {} satisfies EdgeTypes;
